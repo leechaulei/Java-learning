47d57d001dd3b7b9d997fdf84043962a34657525
李秋莉 <dear.123.567@163.com>
2017-07-09 00:28:15 +0800

    var
    var

---
+++ my java/var/var.java
@@ -0,0 +1,17 @@
+public class var {
+	static public void main (String [] args) {
+		byte b;
+		int  i;
+		double d = 57.246;
+		float  f = 12.1f;
+		char   c;
+		b = -128;
+		i = 2586;
+		c = 98;
+		System.out.print("print b =" + b + ", print i =" + i);
+		System.out.println("\nprintln d =" + d  + ", println f =" + f);
+		System.out.printf("\nprintf b = %d", b);
+		System.out.printf("\nprintf i = %d", i);
+		System.out.printf("\nprintf c = %c", c);
+	}
+}
